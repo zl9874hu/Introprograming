258b0599b9e05b49a42d2459429b7587469803ce
zl9874hu <32880721+zl9874hu@users.noreply.github.com>
2017-10-27 10:59:25 -0500

    Update 7.7.py
    Could use some cleaniing but is functional

--- 7.7.py
+++ 7.7.py
@@ -15,12 +15,12 @@ data_point=input('Enter a data point (-1 to stop input):\n')
 while data_point != '-1':#inputting -1 stops loop
     d=data_point.split(',')
     if len(d) >1:
-        d[1]=d[1].replace(' ','')
+        d[1]=d[1].replace(' ','')#removes double spacing
     if len(d) == 1:
         print('Error: No comma in string.\n')
     elif len(d) >= 3:
         print('Error: Too many commas in input.\n')
-    elif d[1].isdigit() != True:
+    elif d[1].isdigit() != True:#if d[1] is not a didgit
         print('Error: Comma not followed by an integer.\n')
     else:
         print('Data string:',d[0])
@@ -36,7 +36,7 @@ print('-'*44)#makes the horizontal line
 for points in range(0,(len(data)-math.ceil((len(data)/2)))):#subtracting the data divided by 2 and rounding up prevents index errors
     print('%-20s|%23s'%(data[i],data[i+1]))#inputs the data
     i=i+2
-i=0
+i=0#sets i back to 0
 print()
 for x in range(1,len(data),2):#from 1 to len(data) counting by 2
     data[x] = int(data[x])#converts every other index to an interger
