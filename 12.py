16768694f46b9afd9ac6d387f99a16d7d922e214
MrDarkSpyro <33063079+MrDarkSpyro@users.noreply.github.com>
2017-10-31 10:14:11 -0500

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,3 +1,11 @@
 pig_latin=[]#creates empty list
 sentence=input('Input your sentance.')
 words=sentence.split(' ')#splits sentence into words
+for i in range(len(words)):
+    word= words[i]
+    word= word[1:] + word[0] + "ay"
+    pig_latin.append(word)
+pig_latin=' '.join(pig_latin)
+print(sentence)
+print(pig_latin)
+#Derek did loop and print
