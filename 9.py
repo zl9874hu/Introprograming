1229591dd4295f14c201763826de39e41b09c7fe
zl9874hu <32880721+zl9874hu@users.noreply.github.com>
2017-10-31 08:21:49 -0500

    Delete 9.py

--- 9.py
+++
@@ -1,9 +0,0 @@
-def letter_counter(string):
-    vowels=['a','e','i','u']
-    if ' ' in string:
-        string=string.replace(' ','')
-    num_lett=len(string)
-    for letter in vowels:
-        string=string.replace(letter,'')
-    num_cons=len(string)
-    num_vowels=num_lett-num_cons
