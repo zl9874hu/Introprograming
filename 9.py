194fa3125d57da637933f2363afdbd07a12978c4
MrDarkSpyro <33063079+MrDarkSpyro@users.noreply.github.com>
2017-10-31 09:55:21 -0500

    Update 9.py

--- 9.py
+++ 9.py
@@ -7,3 +7,10 @@ def letter_counter(string):
         string=string.replace(letter,'')
     num_cons=len(string)
     num_vowels=num_lett-num_cons
+    return num_vowels, num_cons
+
+string=input('Please put in a string:')
+vow,cons=letter_counter(string)
+letter_counter(string)
+print('There are',vow,'vowels, and',cons,'consonants.')
+#Return and print/input done by Derek
