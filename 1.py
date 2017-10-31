04ba1c4793e2f030557df64674d340e32fe21342
MrDarkSpyro <33063079+MrDarkSpyro@users.noreply.github.com>
2017-10-31 09:10:05 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,6 @@
 name=input('What is your full name?')
 print(name)
 name=name.split()#splits the name into 3 parts
+print(name[0][0])
+print('{0}. {1}. {2}.'.format(name[0][0],name[1][0],name[2][0]))
+#Worked on together
