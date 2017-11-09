3d6c06bb31f74485fcdad813b8a4df31dac8b7e5
zl9874hu <32880721+zl9874hu@users.noreply.github.com>
2017-11-09 15:43:54 -0600

    Add files via upload
    Needs some bits added

---
+++ 1.py
@@ -0,0 +1,26 @@
+class cell_phone:
+    def __init__:
+    def set_manufact:
+    def set_model(self):
+    def set_retail_price(self):
+    def get_manufact(self):
+    def get_model(self):
+    def get_retail_price(self):
+def print_menu(option):
+    print('0 to input all details\n1 to set manufacturer\n2 to set model\n3 to set price\n\
+4 to get manufacturer\n5 to get model\n6 to get price\n7 to get phone details\nq to quit')
+    while option !='q':
+        if option is '0':
+        if option is'1':
+        if option is'2':
+        if option is'3':
+        if option is'4':
+        if option is'5':
+        if option is'6':
+        if option is'7':
+            print('you entered')
+        option=input('select an option')
+    return option
+
+option=''
+print_menu(option)
